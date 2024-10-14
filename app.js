5d21cd5a1940e5412b29be0c2c501b0d0f059422
George-25 <georgemallory26@gmail.com>
2024-10-14 15:33:45 -0400

    arry destructuring

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ const nums = [13, 87, 2, 89, 12, 4, 90, 63];
 
 // Create a new array where each value is multiplied by 2 and log the new array.
 const newNums = nums.map((num) => {
- return num * 2;
+  return num * 2;
 });
-console.log(newNums);
-////////
+// console.log(newNums);
+
+// Given the following array, use destructuring to pull out the first and second values and place them into variables. Log both variables.
+
+const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+
+// Your code here
+const [firstTopping, secondTopping] = pizzaToppings;
+console.log(firstTopping);
+console.log(secondTopping);
